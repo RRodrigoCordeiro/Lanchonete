4b5573af772fbff4cdf6cea90e84fb349b09301c
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-07-31 11:38:49 -0300

    Atualização no cardápio 	new file:   img/car.png 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+
 const AdicionarMaisOpcoes = () => {
-    document.getElementById('saiba-mais').addEventListener('click', function () {
+    document.getElementById('saiba-mais').addEventListener('click',function() {
         var opcao = document.getElementById('maisOpcoes')
         if(opcao.classList.contains('hidden')){
             opcao.classList.remove('hidden')
-            this.textContent = 'saiba menos'
+            this.textContent = "Mostrar menos"
         } else {
             opcao.classList.add('hidden')
-            this.textContent ='saiba mais'
-            window.location.href = 'cardapio.html';   
+            this.textContent = "Mostrar mais"
+            // window.location.href = 'cardapio.html'; 
         }
     })
 }
